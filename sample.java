26151bb955534cf1345ee40b5dbf994a6f7cfb01
Brijithk <105113563+Brijithk@users.noreply.github.com>
2024-09-01 07:55:08 -0700

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class sample {
+    public static void main(String[] args) {
+        // Create a list of strings using List.of()
+        List<String> colors = List.of("Red", "Green", "Blue");
+
+        // Print the list
+        System.out.println("Colors: " + colors);
+    }
+}
